92735e2078749f324ad19626c8b294306e64f236
Rajesh Navagare <rajeshnavagare@Rajeshs-MacBook-Pro.local>
2025-04-26 21:10:57 +0100

    Changes for improvisation in specs & cleaned code

--- calculator.rb
+++ calculator.rb
@@ -19,26 +19,21 @@ class Calculator
 
   private
 
-  attr_reader :number_string
-  attr_accessor :delimiter, :valid_number_string, :numbers
+  attr_accessor :delimiter, :number_string, :numbers
 
   def extract_numbers
     extract_number_string_and_delimiter
 
-    self.numbers = valid_number_string
-      .split(regex)
-      .map { |number| unmask_negative_numbers(number) }
-      .map(&:to_i)
+    self.numbers = number_string.split(regex).map { |number| unmask_negative_numbers(number) }.map(&:to_i)
   end
 
   def extract_number_string_and_delimiter
-    self.valid_number_string = number_string
     self.delimiter = DEFAULT_DELIMITER
 
     if number_string.start_with?('//')
       delimiter_string, numbers_part = number_string.split(NEW_LINE_CHARACTER, 2)
       self.delimiter = delimiter_string[2]
-      self.valid_number_string = mask_negatives_for_hyphen_as_delimiter(numbers_part)
+      self.number_string = mask_negatives_for_hyphen_as_delimiter(numbers_part)
     end
   end
 
@@ -51,12 +46,12 @@ class Calculator
   end
 
   def validate_argument_type
-    raise ArgumentError unless @number_string.is_a?(String)
+    raise ArgumentError, 'Input must be a string' unless number_string.is_a?(String)
   end
 
   def validate_numbers
-    negative_number = numbers.find(&:negative?)
-    raise "negative numbers not allowed #{negative_number}" if negative_number
+    negative_numbers = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negative_numbers.join(', ')}" unless negative_numbers.empty?
   end
 
   def regex
