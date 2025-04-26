165d8b3152cc5f0c0add85982e35e666f66f0e0c
Rajesh Navagare <rajeshnavagare@Rajeshs-MacBook-Pro.local>
2025-04-26 20:54:25 +0100

    Added logic to throw exception for the input with negative numbers

--- calculator.rb
+++ calculator.rb
@@ -12,24 +12,26 @@ class Calculator
   end
 
   def add
-    extract_number_string
-    get_numbers_array.sum
+    extract_numbers
+    validate_numbers
+    numbers.sum
   end
 
   private
 
   attr_reader :number_string
-  attr_accessor :delimiter, :valid_number_string
+  attr_accessor :delimiter, :valid_number_string, :numbers
 
-  def validate_argument_type
-    raise ArgumentError unless @number_string.is_a?(String)
+  def extract_numbers
+    extract_number_string_and_delimiter
+
+    self.numbers = valid_number_string
+      .split(regex)
+      .map { |number| unmask_negative_numbers(number) }
+      .map(&:to_i)
   end
 
-  def get_numbers_array
-    valid_number_string.split(regex).map { |number| unmask_negative_numbers(number) }.map(&:to_i)
-  end
-
-  def extract_number_string
+  def extract_number_string_and_delimiter
     self.valid_number_string = number_string
     self.delimiter = DEFAULT_DELIMITER
 
@@ -46,6 +48,15 @@ class Calculator
 
   def unmask_negative_numbers(number)
     number.gsub('~', '-')
+  end
+
+  def validate_argument_type
+    raise ArgumentError unless @number_string.is_a?(String)
+  end
+
+  def validate_numbers
+    negative_number = numbers.find(&:negative?)
+    raise "negative numbers not allowed #{negative_number}" if negative_number
   end
 
   def regex
