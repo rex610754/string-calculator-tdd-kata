bb1cd31f4e9a8219f7ebe91872a7c1b375b3c4eb
Rajesh Navagare <rajeshnavagare@Rajeshs-MacBook-Pro.local>
2025-04-26 19:18:32 +0100

    Added test cases to handle new line characters

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,19 +5,26 @@ RSpec.describe Calculator do
   describe '.add' do
     context 'when input is an empty string' do
       it 'should return 0' do
-        expect(Calculator.add('')).to eq(0)
+        expect(described_class.add('')).to eq(0)
       end
     end
 
     context 'when input is a single number' do
       it 'should return that number as integer' do
-        expect(Calculator.add('7')).to eq(7)
+        expect(described_class.add('7')).to eq(7)
       end
     end
 
     context 'when input is two numbers separated by comma' do
       it 'should return their sum' do
-        expect(Calculator.add('3,5')).to eq(8)
+        expect(described_class.add('3,5')).to eq(8)
+      end
+    end
+
+    context 'when numbers separated by commas and newlines' do
+      it 'should return the sum of multiple numbers' do
+        expect(described_class.add("4\n3,5")).to eq(12)
+        expect(described_class.add("\n\n4,3,5")).to eq(12)
       end
     end
   end
